60bfed9738bf1649c1d5d1066b767d15a84b577d
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-07-26 11:29:23 +0400

    addLaboratoryStaff Method Done

--- Healthcare-Laboratory-Equipment-Reservation-System/src/test/java/com/example/Healthcare/Laboratory/Equipment/Reservation/System/Services/LaboratoryStaffService.java
+++ Healthcare-Laboratory-Equipment-Reservation-System/src/test/java/com/example/Healthcare/Laboratory/Equipment/Reservation/System/Services/LaboratoryStaffService.java
@@ -1,4 +1,30 @@
 package com.example.Healthcare.Laboratory.Equipment.Reservation.System.Services;
 
+import com.example.Healthcare.Laboratory.Equipment.Reservation.System.Entities.LaboratoryStaff;
+import com.example.Healthcare.Laboratory.Equipment.Reservation.System.Repositories.LaboratoryStaffRepository;
+import com.example.Healthcare.Laboratory.Equipment.Reservation.System.Repositories.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LaboratoryStaffService {
+    LaboratoryStaffRepository laboratoryStaffRepository;
+    ReservationRepository reservationRepository;
+
+    @Autowired
+    public LaboratoryStaffService(LaboratoryStaffRepository laboratoryStaffRepository, ReservationRepository reservationRepository) {
+        this.laboratoryStaffRepository = laboratoryStaffRepository;
+        this.reservationRepository = reservationRepository;
+    }
+
+    public LaboratoryStaff addLaboratoryStaff(LaboratoryStaff laboratoryStaff){
+        LaboratoryStaff newLaboratoryStaff = new LaboratoryStaff();
+        newLaboratoryStaff.setName(laboratoryStaff.getName());
+        newLaboratoryStaff.setPhone(laboratoryStaff.getPhone());
+        newLaboratoryStaff.setEmail(laboratoryStaff.getEmail());
+        newLaboratoryStaff.setDepartment(laboratoryStaff.getDepartment());
+        newLaboratoryStaff.setIsActive(laboratoryStaff.getIsActive());
+        laboratoryStaffRepository.save(newLaboratoryStaff);
+        return newLaboratoryStaff;
+    }
 }
