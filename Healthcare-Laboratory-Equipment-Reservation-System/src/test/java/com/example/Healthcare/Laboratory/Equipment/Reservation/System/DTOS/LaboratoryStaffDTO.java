235dcea33d0028adf8beb08d2573fce5164a6c12
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-07-26 11:02:24 +0400

    LaboratoryStaffDTO is created

--- Healthcare-Laboratory-Equipment-Reservation-System/src/test/java/com/example/Healthcare/Laboratory/Equipment/Reservation/System/DTOS/LaboratoryStaffDTO.java
+++ Healthcare-Laboratory-Equipment-Reservation-System/src/test/java/com/example/Healthcare/Laboratory/Equipment/Reservation/System/DTOS/LaboratoryStaffDTO.java
@@ -1,4 +1,29 @@
 package com.example.Healthcare.Laboratory.Equipment.Reservation.System.DTOS;
 
+import com.example.Healthcare.Laboratory.Equipment.Reservation.System.Entities.LaboratoryStaff;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LaboratoryStaffDTO {
+    private Integer id;
+    private String name;
+    private String email;
+    private String phone;
+    private String department;
+    private Boolean isActive;
+
+    public static LaboratoryStaffDTO fromEntity(LaboratoryStaff laboratoryStaff) {
+        LaboratoryStaffDTO dto = new LaboratoryStaffDTO();
+        dto.setId(laboratoryStaff.getId());
+        dto.setName(laboratoryStaff.getName());
+        dto.setEmail(laboratoryStaff.getEmail());
+        dto.setPhone(laboratoryStaff.getPhone());
+        dto.setDepartment(laboratoryStaff.getDepartment());
+        dto.setIsActive(laboratoryStaff.getIsActive());
+        return dto;
+    }
 }
