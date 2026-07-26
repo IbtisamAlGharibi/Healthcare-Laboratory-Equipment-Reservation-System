326e289d6f34a8af979d96747367d5bec9a9d7fa
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-07-26 12:34:55 +0400

    createReservation  Method Done

---
+++ Healthcare-Laboratory-Equipment-Reservation-System/src/test/java/com/example/Healthcare/Laboratory/Equipment/Reservation/System/Services/ReservationService.java
@@ -0,0 +1,32 @@
+package com.example.Healthcare.Laboratory.Equipment.Reservation.System.Services;
+
+import com.example.Healthcare.Laboratory.Equipment.Reservation.System.Entities.LaboratoryStaff;
+import com.example.Healthcare.Laboratory.Equipment.Reservation.System.Entities.Reservation;
+import com.example.Healthcare.Laboratory.Equipment.Reservation.System.Repositories.LaboratoryStaffRepository;
+import com.example.Healthcare.Laboratory.Equipment.Reservation.System.Repositories.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationService {
+
+    ReservationRepository  reservationRepository;
+    LaboratoryStaffRepository   laboratoryStaffRepository;
+    @Autowired
+    public ReservationService(ReservationRepository reservationRepository, LaboratoryStaffRepository   laboratoryStaffRepository) {
+        this.reservationRepository = reservationRepository;
+        this.laboratoryStaffRepository = laboratoryStaffRepository;
+    }
+
+    public Reservation createReservation(Reservation reservation, Integer staffId) {
+        LaboratoryStaff laboratoryStaff = laboratoryStaffRepository.findByLaboratoryStaffId(staffId);
+        Reservation newReservation = new Reservation();
+       newReservation.setLaboratoryStaff(laboratoryStaff);
+       newReservation.setReservationDate(reservation.getReservationDate());
+       newReservation.setStatus(reservation.getStatus());
+       newReservation.setStartTime(reservation.getStartTime());
+       newReservation.setEndTime(reservation.getEndTime());
+       newReservation.setPurpose(reservation.getPurpose());
+       return reservationRepository.save(newReservation);
+    }
+}
