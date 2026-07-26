e9b4a0428dae92c493be32535fb6e2acde2560df
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-07-26 10:51:41 +0400

    ReservationRepository is created

--- Healthcare-Laboratory-Equipment-Reservation-System/src/test/java/com/example/Healthcare/Laboratory/Equipment/Reservation/System/Repositories/LaboratoryStaffRepository.java
+++ Healthcare-Laboratory-Equipment-Reservation-System/src/test/java/com/example/Healthcare/Laboratory/Equipment/Reservation/System/Repositories/LaboratoryStaffRepository.java
@@ -8,15 +8,15 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface LaboratoryStaffRepository extends JpaRepository<LaboratoryStaff,Integer> {
-    @Query("select ls from LaboratoryStaff where ls.isActive=true")
+    @Query("select ls from LaboratoryStaff ls where ls.isActive=true")
     List<LaboratoryStaff> getAllStaff();
 
-    @Query("select ls from LaboratoryStaff where ls.isActive=true and ls.id =:id")
+    @Query("select ls from LaboratoryStaff ls where ls.isActive=true and ls.id =:id")
     LaboratoryStaff findByLaboratoryStaffId(@Param("id") Integer id);
 
-    @Query("select ls from LaboratoryStaff where ls.isActive=true and ls.name =:name")
+    @Query("select ls from LaboratoryStaff ls where ls.isActive=true and ls.name =:name")
     LaboratoryStaff findByLaboratoryStaffName(@Param("name") String name);
 
-    @Query("select ls from LaboratoryStaff where ls.isActive=true and ls.department =:department")
+    @Query("select ls from LaboratoryStaff ls where ls.isActive=true and ls.department =:department")
     List<LaboratoryStaff> findByDepartment(@Param("department") String department);
 }
---
+++ Healthcare-Laboratory-Equipment-Reservation-System/src/test/java/com/example/Healthcare/Laboratory/Equipment/Reservation/System/Repositories/ReservationRepository.java
@@ -0,0 +1,25 @@
+package com.example.Healthcare.Laboratory.Equipment.Reservation.System.Repositories;
+
+import com.example.Healthcare.Laboratory.Equipment.Reservation.System.Entities.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+    @Query("select r from Reservation r where r.laboratoryStaff.id=:staffId ")
+    List<Reservation> findByLaboratoryStaffId(@Param("staffId") Integer staffId);
+    @Query("select r from Reservation r where r.equipment.id=:equipmentId ")
+    List<Reservation> findByEquipmentId(@Param("equipmentId") Integer equipmentId);
+    @Query("select r from Reservation r where r.status=:status ")
+    List<Reservation> findByStatus(@Param("status") String  status);
+    @Query("select r from Reservation r where r.reservationDate=:reservationDate ")
+    List<Reservation> findByReservationDate(@Param("reservationDate") LocalDate reservationDate);
+    @Query("select r from Reservation r where r.status='Approved'")
+    List<Reservation> getApprovedReservations();
+    @Query("SELECT r FROM Reservation r WHERE r.laboratoryStaff.id =:staffId AND r.status =:status")
+    List<Reservation> findByLaboratoryStaffIdAndStatus(@Param("staffId") Integer staffId, @Param("status") String status);
+
+}
